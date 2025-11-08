da0180e2ba31ce4d7ffe33b072e648b5be36dc19
Matej Bocko <matej.bocko@aston.sk>
2025-11-08 22:36:23 +0100

    version

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "org.javamaster"
-version = "5.8.4"
+version = "6.0.3-ASTON"
 
 repositories {
     maven { url = URI("https://maven.aliyun.com/nexus/content/groups/public/") }
